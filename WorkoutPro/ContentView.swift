4e74c26da0f0b1bcf586a0c3893c631e667ae7d4
justin2213 <jrgmys@gmail.com>
2024-06-03 12:48:54 -0400

    Initial Commit

---
+++ WorkoutPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutPro
+//
+//  Created by Justin Gmys on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutPro/WorkoutProApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutProApp.swift
+//  WorkoutPro
+//
+//  Created by Justin Gmys on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
